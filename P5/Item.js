3743ec0cfed7a583128ebf7bf87de28e8f7cce41
androia <andreaarubio@gmail.com>
2016-10-21 07:45:28 +0200

    Update Item.js

--- P5/Item.js
+++ P5/Item.js
@@ -1,16 +1,7 @@
-var Item = function Item(imagen, titol, raza, color, localitzacio){
+var Item = function Item(imagen, raza, color){
     this.image = imagen;
-    this.title = titol;
-    this.race = raza;
+    this.title = raza;
     this.color = color;
-    this.location = localitzacio;
 };
-
-// Get random price in range min, max
-/*Item.prototype.getPrice = function(){
-    var min = 100;
-    var max = 1000;
-    return (Math.random() * (max - min) + min).toFixed(2);
-}*/
 
 exports.Item = Item;
