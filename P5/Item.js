4ea63587324067367a59f5ee8a486377b2e4510d
androia <andreaarubio@gmail.com>
2016-10-21 18:04:04 +0200

    Update Item.js

--- P5/Item.js
+++ P5/Item.js
@@ -1,7 +1,9 @@
-var Item = function Item(imagen, raza, color){
+var Item = function Item(imagen, nom, raça, color, edat){
     this.image = imagen;
-    this.title = raza;
+    this.name = nom;
+    this.title = raça;
     this.color = color;
+    this.age = edat;
 };
 
 exports.Item = Item;
