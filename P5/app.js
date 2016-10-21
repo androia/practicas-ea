9fbf1d859734cc5edc7c9ecff316a85f0bec8ed2
androia <andreaarubio@gmail.com>
2016-10-21 18:04:23 +0200

    Update app.js

--- P5/app.js
+++ P5/app.js
@@ -26,16 +26,41 @@ api500px.photos.getPopular({'sort': 'created_at', 'rpp': '10','image_size':200},
     });
 });*/
 
+// Use 500px API to get random pictures for our products
+/*var npmSearch = require('500px');
+var search = npmSearch(__dirname + '/npm.json');
+var pics = [];
+var query = process.argv.slice(2).join(' ');
+search(query,  function(error, results) {
+    // Do something
+    pics = results.forEach(function(a){
+        // Compose object to be used in show items template
+        return new Item(a.image_url);
+    });
+});*/
+
+/*var API500px = require('500px');
+var api500px = new API500px("YecP85RjzG08DN0MqvgFa0N780dNaDmJX6iTPbYp");
+var pics = [];
+var query = process.argv.slice(2).join(' ');
+api500px.search({'sort': 'created_at', 'rpp': '10','image_size':200}, query, function(error, results) {
+    // Do something
+    pics = results.forEach(function(a){
+        // Compose object to be used in show items template
+        return new Item(a.image_url);
+    });
+});*/
+
 var pics = [ //imagen, raza, color
-    new Item("public/img/aHe45yi.jpg", "Pastor Alemany", "MARRÓ"),
-    new Item("public/img/gato-y-fondo-negro-1807.jpg", "Manx", "NEGRE"),
-    new Item("public/img/644189_angliyskiy-buldog_buldog_sobaka_chrnyiy-fon_5223x3482_www.Gde-Fon.com.jpg", "BullDog", "MARRÓ", "Estimem"),
-    new Item("public/img/298995-alexfas01.jpg", "Golden Retriever", "MARRÓ"),
-    new Item("public/img/Cats_Black_background_498711.jpg", "Siberiano", "MARRÓ"),
-    new Item("public/img/dog_puppy_black_baby_1174_1920x1080.jpg", "Labrador", "NEGRE"),
-    new Item("public/img/eyes-black-dogs-funny-monochrome-black-background-200896.jpg", "Weimaraner", "GRIS"),
-    new Item("public/img/Black-cat-green-eyes-black-background_2560x1440.jpg", "Chartreux", "GRIS"),
-    new Item("public/img/fon-chernyy-schenok-drug-sobaka.jpg", "Bòxer", "MARRÓ")
+    new Item("public/img/aHe45yi.jpg", "THOR", "Pastor Alemany", "marró", "7"),
+    new Item("public/img/gato-y-fondo-negro-1807.jpg", "FURA", "Manx", "negre", "3"),
+    new Item("public/img/644189_angliyskiy-buldog_buldog_sobaka_chrnyiy-fon_5223x3482_www.Gde-Fon.com.jpg", "ARGO", "Bulldog", "marró", "8"),
+    new Item("public/img/298995-alexfas01.jpg", "GALA", "Golden Retriever", "marró", "3"),
+    new Item("public/img/Cats_Black_background_498711.jpg", "SIAM", "Siberiano", "marró", "5"),
+    new Item("public/img/dog_puppy_black_baby_1174_1920x1080.jpg", "VENOM", "Labrador", "negre", "1"),
+    new Item("public/img/eyes-black-dogs-funny-monochrome-black-background-200896.jpg", "KIRA", "Weimaraner", "gris", "1"),
+    new Item("public/img/Black-cat-green-eyes-black-background_2560x1440.jpg", "SPIUK", "Chartreux", "gris", "3"),
+    new Item("public/img/fon-chernyy-schenok-drug-sobaka.jpg", "NEO", "Bòxer", "marró", "1")
 ];
 
 // Render frontpage
